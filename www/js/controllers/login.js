0205cde06042310e55f45ee5aa005ae0cd081933
clouie87 <carol.louie@gmail.com>
2015-11-28 15:23:54 -0400

    robs test for mobile

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -1,11 +1,28 @@
 'use strict';
 
-app.controller('LoginCtrl', function(FURL, $scope, Auth) {
+app.controller('LoginCtrl', function(FURL, $scope, Auth, $state) {
+  console.log('brians line');
   $scope.twitterLogin = function(){
+
     console.log('twitter clicked');
-    Auth.login().then(function(){
-      console.log('will redirect to');
-    });
+    Auth.$authWithOAuthRedirect('twitter').then(function(authData) {
+      console.log('we are in the authwithoauth');
+      $state.go('dash');
+      }).catch(function(error) {
+        if (error.code === 'TRANSPORT_UNAVAILABLE') {
+          Auth.$authWithOAuthPopup('twitter').then(function (error, authData) {
+            if (error) {
+              console.log("Login Failed!", error);
+            } else {
+
+              console.log("Authenticated successfully with payload:", authData);
+              $state.go('dash');
+              //return Auth.createProfile(authData);
+            }
+          });
+        }
+      });
+
   };
 
 });
--- www/js/services/auth.js
+++ www/js/services/auth.js
@@ -4,50 +4,51 @@ app.factory('Auth', function(FURL, $firebaseAuth, $firebaseObject, $state) {
   var ref = new Firebase(FURL);
   var auth = $firebaseAuth(ref);
 
-  var Auth = {
-
-    user: {},
-
-    createProfile: function(user){
-      console.log('in createProfile', user);
-        var profile ={
-          name: user.twitter.displayName,
-          gravatar: user.twitter.profileImageURL
-        };
-      console.log(profile);
-
-      return ref.child('profile').child(user.uid).set(profile);
-    },
-
-
-    login: function() {
-      console.log("we got to login function");
-      ref.$authWithOAuthRedirect('twitter').then(function (authData) {
-      }).catch(function (error) {
-        if (error.code === 'TRANSPORT_UNAVAILABLE') {
-          ref.$authWithOAuthPopup('twitter').then(function (authData) {
-            if (error) {
-              console.log("Login Failed!", error);
-            } else {
-              console.log("Authenticated successfully with payload:", authData);
-              return Auth.createProfile(authData);
-            }
-          });
-        }
-      })
-    },
-
-    logout: function(){
-      auth.$unauth();
-    }
-  };
-
+  //var Auth = {
+  //
+  //  user: {},
+  //
+  //  createProfile: function(user){
+  //    console.log('in createProfile', user);
+  //      var profile ={
+  //        name: user.twitter.displayName,
+  //        gravatar: user.twitter.profileImageURL
+  //      };
+  //    console.log(profile);
+  //
+  //    return ref.child('profile').child(user.uid).set(profile);
+  //  },
+  //
+  //
+  //  //login: function(){
+  //  //  console.log("we got to login function");
+  //  //  ref.authWithOAuthRedirect('twitter', function(authData) {
+  //  //
+  //  //  }).catch(function(error) {
+  //  //    if (error.code === 'TRANSPORT_UNAVAILABLE') {
+  //  //      ref.authWithOAuthPopup('twitter', function (error, authData) {
+  //  //        if (error) {
+  //  //          console.log("Login Failed!", error);
+  //  //        } else {
+  //  //
+  //  //          console.log("Authenticated successfully with payload:", authData);
+  //  //          return Auth.createProfile(authData);
+  //  //        }
+  //  //      });
+  //  //    }
+  //  //  });
+  //  //},
+  //
+  //  logout: function(){
+  //    auth.$unauth();
+  //  }
+  //};
+  //
   auth.$onAuth(function(authData){
     if(authData){
-      Auth.user=authData;
-      Auth.user.profile = $firebaseObject(ref.child('profile').child(authData.uid));
+      //Auth.user=authData;
+      //Auth.user.profile = $firebaseObject(ref.child('profile').child(authData.uid));
       console.log('the user has already logged in');
-      //return auth;
       $state.go('dash');
     }else {
       $state.go('login');
@@ -55,6 +56,6 @@ app.factory('Auth', function(FURL, $firebaseAuth, $firebaseObject, $state) {
 
   });
 
-  return Auth;
+  return auth;
 });
 
