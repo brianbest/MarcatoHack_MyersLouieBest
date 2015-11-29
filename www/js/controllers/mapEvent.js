98216f7c4640d4b85ab05dd966ac2cca471926e7
robmyers82 <robert_myers_@hotmail.com>
2015-11-29 14:58:36 -0400

    Dynamic map functionality added.

--- www/js/controllers/mapEvent.js
+++ www/js/controllers/mapEvent.js
@@ -1,20 +1,113 @@
 angular.module('starter.MapEventCtrl', [])
 
-  .controller('MapEventCtrl', function($scope, $ionicSlideBoxDelegate, $cordovaGeolocation, Auth, Event, $stateParams, Coords, $state){
+  .controller('MapEventCtrl', function(FURL, $scope, $ionicSlideBoxDelegate, $cordovaGeolocation, $firebaseObject, Auth, Event, $stateParams, Coords, $state){
+    var ref = new Firebase(FURL);
+    $scope.lat = 0.0;
+    $scope.lng = 0.0;
     var eventId = $stateParams.eventId;
-    //var userId = $stateParams.userId;
-    //console.log(userId);
-    console.log(eventId);
+    var markers = [];
+
+    window.setInterval(function() {
+      Coords.updateCoords().then(function(coords) {
+        $scope.lat = coords.lat;
+        $scope.lng = coords.lng;
+      });
+    }, 10000);
+
+    var options = {timeout: 10000, enableHighAccuracy: true};
+
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+      console.log(position.coords.latitude);
+      $scope.x = position.coords.latitude;
+      $scope.y = position.coords.longitude;
+
+      // center of map
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+        // var marker = new google.maps.Marker({
+        //   map: $scope.map,
+        //   position: latLng
+        // });
+
+
+        if(eventId) {
+          Event.getEventgoers(eventId).$loaded().then(function(goers){
+            $scope.goers = goers.length;
+
+            for (var i = 0; i < goers.length; i++) {
+              var theUserId = goers.$keyAt(i);
+              $firebaseObject(ref.child('coords').child(theUserId)).$loaded().then(function(coordinates) {
+
+                var newLatLng = new google.maps.LatLng(coordinates.lat, coordinates.lng);
+                var marker = new google.maps.Marker({
+                  map: $scope.map,
+                  position: newLatLng
+                });
+
+                markers.push(marker);
+              }); 
+            }
+
+            console.log('loaded!');
+
+            window.setInterval(function() {
+
+              // Call set markers to re-add markers
+              Event.getEventgoers(eventId).$loaded().then(function(newgoers){
+                // Loop through markers and set map to null for each
+                for (var j = 0; j < markers.length; j++) {
+                    markers[j].setMap(null);
+                }
+
+                // Reset the markers array
+                markers = [];
+
+                $scope.newgoers = newgoers.length;
+
+                for (var k = 0; k < newgoers.length; k++) {
+                  var theUserId = newgoers.$keyAt(k);
+                  $firebaseObject(ref.child('coords').child(theUserId)).$loaded().then(function(coordinates) {
+
+                    var newLatLng = new google.maps.LatLng(coordinates.lat, coordinates.lng);
+                    var marker = new google.maps.Marker({
+                      map: $scope.map,
+                      position: newLatLng
+                    });
+
+                    markers.push(marker);
+                  }); 
+                }
+              });
+              
+            }, 10000);
+          });
+        }
+        else {
+          var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: latLng
+          });
+        }
+
+      });
+
+    }, function(error){
+      console.log("Could not get location");
+    });
 
     if(eventId) {
       $scope.showJoinEvent = true;
       $scope.showAddEvent = false;
-      Event.getEventgoers(eventId).$loaded().then(function(goers){
-        console.log(goers.length);
-        console.log(goers);
-        $scope.goers = goers.length;
-      });
-
 
       Event.getEvent(eventId).$loaded().then(function(event){
         console.log(event);
